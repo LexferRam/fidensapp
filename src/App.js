dd7e6d1cb0fb566dda86c51899d4695f59e8c736
Lexfer Ramirez <lexferramirez@gmail.com>
2021-05-03 15:04:53 -0400

    agregando pantalla de login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from "react";
-import { Grid, Paper } from "@material-ui/core";
-import { TableMaster } from "./components/TableMaster";
+import { AppRouter } from "./router/AppRouter";
 
-import { DatosInputs } from "./components/DatosInputs";
-import SimpleBackdrop from "./components/Backdrop";
-import Dialog from "./components/Dialog";
-
-export default function CollapsibleTable() {
+export const App = () => {
   return (
     <>
-      <Dialog />
-      <SimpleBackdrop />
-      <Paper elevation={5} style={{ margin: 20, padding: 30 }}>
-        <Grid container justify="center" spacing={3}>
-          <Grid item xs={12}>
-            <DatosInputs />
-          </Grid>
-
-          <Grid item xs={12}>
-            <TableMaster />
-          </Grid>
-        </Grid>
-      </Paper>
+      <AppRouter />
     </>
   );
-}
+};
