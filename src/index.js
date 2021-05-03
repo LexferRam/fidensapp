24760cf89c012f20b97c2f5c62cfe83c126f1b8f
Lexfer Ramirez <lexferramirez@gmail.com>
2021-05-03 15:13:15 -0400

    agregando login

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
-import { AuthProvider } from "./auth/AuthContext";
+import App from "./App";
 import { DataProvider } from "./context/DataProvider";
 import "./index.css";
 
 ReactDOM.render(
-  <AuthProvider>
-    <DataProvider>
-      <App />
-    </DataProvider>
-  </AuthProvider>,
+  <DataProvider>
+    <App />
+  </DataProvider>,
   document.getElementById("root")
 );
